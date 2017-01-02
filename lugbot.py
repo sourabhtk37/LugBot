fdeccea3d9b595470b542c1ed95b4b8f61f18dd9
T K Sourabh <sourabhtk37@gmail.com>
2017-01-02 23:44:55 +0530

    twitter & facebook link added

--- lugbot.py
+++ lugbot.py
@@ -12,6 +12,8 @@ def home():
 
 invite_link = "https://telegram.me/joinchat/DQQdYgqEmJF62rPE__9d_w"
 mailing_list = "http://frodo.hserus.net/mailman/listinfo/ilugd"
+fb_link = "https://www.facebook.com/ILUGD/"
+tw_link = "https://twitter.com/ilugdelhi"
 
 if __name__ == '__main__':
 
@@ -24,6 +26,10 @@ if __name__ == '__main__':
             bot.sendMessage(chat_id, invite_link)
         if msg['text'] == '/mailinglist':
             bot.sendMessage(chat_id, mailing_list)
+        if msg['text'] == '/facebook':
+            bot.sendMessage(chat_id, fb_link)
+        if msg['text'] == '/twitter':
+            bot.sendMessage(chat_id, tw_link)
 
     bot.message_loop(handle)
     while 1:
